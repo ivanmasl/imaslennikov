a5d1f004438e9a6d536a382406d23e20ea73dc9b
ivanmasl <i.a.maslennikov@gmail.com>
2018-01-29 20:50:09 +0700

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j;
+
+/**
+* Class Класс для печати строки на экране
+* Author imaslennikov
+* @since 29.01.2018
+* @version 1
+*/
 public class Calculate {
+	/**
+	* Печать
+	*/
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
